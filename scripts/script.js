a0a84b820eecf6aec1ec556c0ccd991503503713
Louis Michalak <louis.michalak@epitech.eu>
2019-07-21 15:28:55 +0200

    modal done

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //navbar
     let dropdownbool = false;
     $('.dropdown-menu').hide();
     $(document).click(function (e) {
@@ -25,4 +26,44 @@ $(document).ready(function () {
         $('.navbar-collapse').slideToggle();
         $('.navbar-collapse').css('display', 'flex');
     });
+    //modals
+    $('.btn-modal').click(function () {
+        let modalId = $(this).data("target");
+        $(modalId).modal("show")
+    });
+    $('.popin-dismiss').click(function () {
+        $(this).closest('.modal').modal("hide");
+    });
+    $(document).keyup(function (e) {
+        if (e.keyCode === 27)
+        {
+            let key = searchOpenModal();
+        }
+    });
+    $.fn.modal = function(action) {
+        if (action === "show")
+        {
+            $(this).css('display', 'block');
+            $(this).fadeTo(0,0);
+            $(this).fadeTo('100', 1);
+        }
+        if (action === "hide")
+        {
+            $(this).css('display', 'none');
+        }
+    };
+    $(document).click(function (e) {
+        if ($(e.target).attr('class') === 'modal')
+        {
+            $(e.target).modal('hide');
+        }
+    });
+    function searchOpenModal() {
+        $('.modal').each(function (key, data) {
+            if ($($('.modal')[key]).css('display') == 'block')
+            {
+                $($('.modal')[key]).modal('hide');
+            }
+        });
+    }
 });
